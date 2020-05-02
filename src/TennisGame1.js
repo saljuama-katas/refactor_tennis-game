dc26c540503d37073fc6addfc229083094912607
Salvador Juan <sjuan@thoughtworks.com>
2020-05-02 12:52:47 +0200

    convert to class + get rid of hardcoded player names

--- src/TennisGame1.js
+++ src/TennisGame1.js
@@ -1,63 +1,67 @@
-var TennisGame1 = function (player1Name, player2Name) {
-  this.m_score1 = 0
-  this.m_score2 = 0
-  this.player1Name = player1Name
-  this.player2Name = player2Name
-}
+class TennisGame1 {
+  constructor(player1Name, player2Name) {
+    this.player1Name = player1Name
+    this.player1Score = 0
+    this.player2Name = player2Name
+    this.player2Score = 0
+  }
 
-TennisGame1.prototype.wonPoint = function (playerName) {
-  if (playerName === 'player1') this.m_score1 += 1
-  else this.m_score2 += 1
-}
+  wonPoint(playerName) {
+    if (playerName === this.player1Name)
+      this.player1Score += 1
+    else
+      this.player2Score += 1
+  }
 
-TennisGame1.prototype.getScore = function () {
-  var score = ''
-  var tempScore = 0
-  if (this.m_score1 === this.m_score2) {
-    switch (this.m_score1) {
-      case 0:
-        score = 'Love-All'
-        break
-      case 1:
-        score = 'Fifteen-All'
-        break
-      case 2:
-        score = 'Thirty-All'
-        break
-      default:
-        score = 'Deuce'
-        break
-    }
-  } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-    var minusResult = this.m_score1 - this.m_score2
-    if (minusResult === 1) score = 'Advantage player1'
-    else if (minusResult === -1) score = 'Advantage player2'
-    else if (minusResult >= 2) score = 'Win for player1'
-    else score = 'Win for player2'
-  } else {
-    for (var i = 1; i < 3; i++) {
-      if (i === 1) tempScore = this.m_score1
-      else {
-        score += '-'
-        tempScore = this.m_score2
-      }
-      switch (tempScore) {
+  getScore() {
+    var score = ''
+    var tempScore = 0
+    if (this.player1Score === this.player2Score) {
+      switch (this.player1Score) {
         case 0:
-          score += 'Love'
+          score = 'Love-All'
           break
         case 1:
-          score += 'Fifteen'
+          score = 'Fifteen-All'
           break
         case 2:
-          score += 'Thirty'
+          score = 'Thirty-All'
           break
-        case 3:
-          score += 'Forty'
+        default:
+          score = 'Deuce'
           break
       }
+    } else if (this.player1Score >= 4 || this.player2Score >= 4) {
+      var minusResult = this.player1Score - this.player2Score
+      if (minusResult === 1) score = `Advantage ${this.player1Name}`
+      else if (minusResult === -1) score = `Advantage ${this.player2Name}`
+      else if (minusResult >= 2) score = `Win for ${this.player1Name}`
+      else score = `Win for ${this.player2Name}`
+    } else {
+      for (var i = 1; i < 3; i++) {
+        if (i === 1) tempScore = this.player1Score
+        else {
+          score += '-'
+          tempScore = this.player2Score
+        }
+        switch (tempScore) {
+          case 0:
+            score += 'Love'
+            break
+          case 1:
+            score += 'Fifteen'
+            break
+          case 2:
+            score += 'Thirty'
+            break
+          case 3:
+            score += 'Forty'
+            break
+        }
+      }
     }
+    return score
   }
-  return score
 }
 
 if (typeof window === 'undefined') {
