bee55c7573afd70d1b60ba2a92e7ce79e8ce9d28
Salvador Juan <sjuan@thoughtworks.com>
2020-05-02 15:26:06 +0200

    break down an if that was feeling too smart and grouping to many conditions

--- src/TennisGame3.js
+++ src/TennisGame3.js
@@ -7,13 +7,18 @@ class TennisGame3 {
   }
 
   getScore() {
-    if (this.p1score >= 4 || this.p2score >= 4 || this.p1score + this.p2score >= 6) {
-      if (this.p1score === this.p2score) return 'Deuce'
-      const playerAhead = this.p1score > this.p2score ? this.p1name : this.p2name
-      return Math.abs(this.p1score - this.p2score) === 1
-        ? 'Advantage ' + playerAhead
-        : 'Win for ' + playerAhead
-    }
+    const scoreDifference = Math.abs(this.p1score - this.p2score)
+    const playerAhead = this.p1score > this.p2score ? this.p1name : this.p2name
+
+    const isGameOver = () => (this.p1score >= 4 || this.p2score >= 4) && scoreDifference >= 2
+    if (isGameOver()) return `Win for ${playerAhead}`
+
+    const playerHasAdvantage = () => (this.p1score >= 3 && this.p2score >= 3) && scoreDifference === 1
+    if (playerHasAdvantage()) return `Advantage ${playerAhead}`
+
+    const isDeuce = () => (this.p1score === this.p2score) && this.p1score >= 3
+    if (isDeuce()) return 'Deuce'
+
     const scoreNames = ['Love', 'Fifteen', 'Thirty', 'Forty']
     const p1ScoreValue = scoreNames[this.p1score]
     const p2ScoreValue = scoreNames[this.p2score]
