3a42aab72cc6b83ca9f822d536829ef8f59e9a30
Salvador Juan <sjuan@thoughtworks.com>
2020-05-02 14:30:34 +0200

    remove tons of duplicated code

--- src/TennisGame2.js
+++ src/TennisGame2.js
@@ -15,6 +15,8 @@ class TennisGame2 {
       if (points === 2) return 'Thirty'
       if (points === 3) return 'Forty'
     }
+    this.scoreP1 = playerScoreValue(this.pointsP1)
+    this.scoreP2 = playerScoreValue(this.pointsP2)
 
     if (this.pointsP1 >= 4 && this.pointsP2 >= 0 && this.pointsP1 - this.pointsP2 >= 2)
       return `Win for ${this.nameP1}`
@@ -24,42 +26,11 @@ class TennisGame2 {
       return `Advantage ${this.nameP1}`
     if (this.pointsP2 > this.pointsP1 && this.pointsP1 >= 3)
       return `Advantage ${this.nameP2}`
-
     if (this.pointsP1 === this.pointsP2) {
       if (this.pointsP1 > 2) return 'Deuce'
       else return `${playerScoreValue(this.pointsP1)}-All`
     }
-
-    if (this.pointsP1 > 0 && this.pointsP2 === 0) {
-      if (this.pointsP1 === 1) this.scoreP1 = 'Fifteen'
-      if (this.pointsP1 === 2) this.scoreP1 = 'Thirty'
-      if (this.pointsP1 === 3) this.scoreP1 = 'Forty'
-      this.scoreP2 = 'Love'
-      return this.scoreP1 + '-' + this.scoreP2
-    }
-    if (this.pointsP2 > 0 && this.pointsP1 === 0) {
-      if (this.pointsP2 === 1) this.scoreP2 = 'Fifteen'
-      if (this.pointsP2 === 2) this.scoreP2 = 'Thirty'
-      if (this.pointsP2 === 3) this.scoreP2 = 'Forty'
-      this.scoreP1 = 'Love'
-      return this.scoreP1 + '-' + this.scoreP2
-    }
-
-    if (this.pointsP1 > this.pointsP2 && this.pointsP1 < 4) {
-      if (this.pointsP1 === 2) this.scoreP1 = 'Thirty'
-      if (this.pointsP1 === 3) this.scoreP1 = 'Forty'
-      if (this.pointsP2 === 1) this.scoreP2 = 'Fifteen'
-      if (this.pointsP2 === 2) this.scoreP2 = 'Thirty'
-      return this.scoreP1 + '-' + this.scoreP2
-    }
-    if (this.pointsP2 > this.pointsP1 && this.pointsP2 < 4) {
-      if (this.pointsP2 === 2) this.scoreP2 = 'Thirty'
-      if (this.pointsP2 === 3) this.scoreP2 = 'Forty'
-      if (this.pointsP1 === 1) this.scoreP1 = 'Fifteen'
-      if (this.pointsP1 === 2) this.scoreP1 = 'Thirty'
-      return this.scoreP1 + '-' + this.scoreP2
-    }
-
+    return this.scoreP1 + '-' + this.scoreP2
   }
 
   wonPoint(player) {
